0dc91a8dd8d3ed70cd35940d44f7648e518c856b
Nicolas Larrosa <nicolaslarrosa.mail@gmail.com>
2023-12-08 00:37:36 -0300

    Custom hook useAxios

--- src/Gimoji.jsx
+++ src/Gimoji.jsx
@@ -2,39 +2,73 @@ import { CustomGifcard } from "./components/ui/CustomGifcard";
 import { CustomSearch } from "./components/filters/CustomSearch";
 import { CustomSelect } from "./components/filters/CustomSelect";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { reqAxios } from "./config/axiosGiphy";
+import { useAxios } from "./hooks/useAxios";
 
 
 
-// const noImage = import.meta.env.VITE_NO_IMAGE;
-// const apiKey = import.meta.env.VITE_APIKEY_GIPHY;
-// const urlApi = import.meta.env.VITE_URL_API;
+const apiKey = import.meta.env.VITE_APIKEY_GIPHY;
+
+
 
 
 export const Gimoji = () => {
 
-    const noImage = 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg';
-    const apiKey = 'oivWS7jMDDIhg4T2PB8dqcRp7yroNtuT';
-    const [dataGimoji, setDataGimoji] = useState([]);
-    const [dataCategories, setDataCategories] = useState([]);
+    // const noImage = 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg';
+    // const urlApi = 'https://api.giphy.com/v1/gifs/';
+    // const [dataGimoji, setDataGimoji] = useState([]);
+    // const [dataCategories, setDataCategories] = useState([]);
+    const [textSearch, setTextSearch] = useState('animals')
     const limit = 16;
+    const urlSearch = `search?api_key=${apiKey}&q=${textSearch}&limit=${limit}&offset=0`;
+    const urlCategories = `categories?api_key=${apiKey}`;
 
-    useEffect(() => {
-        getCategories();
-        getGimoji();
-    }, []);
+    const { dataApi: dataGifs } = useAxios(urlSearch);
+    const { dataApi: dataCategories } = useAxios(urlCategories);
+
+    // useEffect(() => {
+    //     getGimoji();
+    // }, [textSearch]);
 
 
-    const getGimoji = async() => {
-        const resp = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=animals&limit=${limit}&offset=0`);
-        const {data} = await resp.json();
-        setDataGimoji(data);
+    // useEffect(() => {
+    //     getCategories();
+    // }, [])
+    
+
+    //BUsca los Gif en el endpoint cada vez que se modifica el estado de setTextSearch
+    // const getGimoji = async() => {
+    //     // const resp = await fetch(`${urlApi}search?api_key=${apiKey}&q=${textSearch}&limit=${limit}&offset=0`);
+    //     // const {data} = await resp.json();
+    //     // setDataGimoji(data);
+
+    //     const resp = await reqAxios.get(`search?api_key=${apiKey}&q=${textSearch}&limit=${limit}&offset=0`);
+    //     const { data } = await resp.data
+    //     setDataGimoji(data);
+
+        
+    // }
+
+    // BUSCA POR UNICA VEZ LAS CATEGORIAS EN EL ENDPOINT
+    // const getCategories = async() => {
+    //     // const resp = await fetch(`${urlApi}categories?api_key=${apiKey}`);
+    //     // const {data} = await resp.json();
+    //     // setDataCategories(data)
+
+    //     const resp = await reqAxios.get(`categories?api_key=${apiKey}`);
+    //     const {data: dataCateg} = await resp.data
+    //     setDataCategories(dataCateg)
+    // }
+
+    const onChangeData = (event) => {
+        setTextSearch(event.target.value);
     }
 
-    const getCategories = async() => {
-        const resp = await fetch(`https://api.giphy.com/v1/gifs/categories?api_key=${apiKey}`);
-        const {data} = await resp.json();
-        setDataCategories(data)
-    }
+
+    const onClickSearch = (text) => {
+        setTextSearch(text);
+      }
     
     
     return (
@@ -44,10 +78,13 @@ export const Gimoji = () => {
                 <div className="col-sm-4">  
                     <CustomSelect 
                         dataOptions={dataCategories}
+                        onChangeData={(event) => onChangeData(event)}
                     />
                 </div>
                 <div className="col-sm-6">
-                   <CustomSearch />
+                   <CustomSearch 
+                        onClickSearch={(value) => onClickSearch(value)}
+                   />
                 </div>
             </div>
         </div> 
@@ -55,7 +92,7 @@ export const Gimoji = () => {
         <div className="album py-md-5 ">
             <div className="container-fluid">
                 <div className="row row-cols-sm-1 row-cols-md-4 g-3">
-                    { dataGimoji.map( gif => (
+                    { dataGifs.map( gif => (
                         <CustomGifcard key={gif.id} dataItem={gif}/>
                     ))}
                 </div>
